b8d311fcd26d60408ea825b2a563cd206ecc4c94
Matt DeBoard <matt.deboard@gmail.com>
2011-03-16 14:53:56 -0400

    More comments for explanation

--- test_pylot.py
+++ test_pylot.py
@@ -26,29 +26,33 @@ class Event(object):
                                                                 self.data3, 
                                                                 self.timestamp)
 
-class Report():
 
-    
-
-
+# sqlalchemy connection to the db
 engine = create_engine("sqlite:///browser_usage_example_results.sqlite")
-
+# defining the table to use for queries, auto-populate with the data from
+# the engine connection
 usage_patterns = Table("usage_patterns", 
                        meta, 
                        autoload=True, 
                        autoload_with=engine)
 
-
+# establish a session class based on the engine connection
 Session = sessionmaker(bind=engine)
+# instantiate the session
 session = Session()
+# bind a metadata object to the db
 meta = MetaData(bind=engine)
+# simple query from the table, sorted by the timestamp field 
+# (.order_by("-timestamp") to sort in reverse). Returned as a list of
+# tuples, one tuple per row.
 q = session.query(usage_patterns).order_by("timestamp")
+# tuple containing column names
 keys = ('event_code', 'data1', 'data2', 'data3', 'timestamp')
-events = [zip(keys,i) for i in q]
-dlist = []
-
-
+# zip the rows in the query with the column headers to get a list of a
+# list of tuples (not a typo... [[(a,b), (c,d)], [(e,f), (g,h)]] ) 
+events = [zip(keys,row) for row in q]
 # Returns a list of dictionaries -- this belongs inside a class.
+dlist = []
 for event in events:
     dlist.append(dict(event))
 event_stream = {}
