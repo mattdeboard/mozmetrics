c1a7306503a0b324b9d32640af7bfa63488c7213
Matt DeBoard <matt.deboard@gmail.com>
2011-03-15 21:59:45 -0400

    Added a few more notes. Trying to just rough out an idea for how the data *might* move from client->server.

--- test_pylot.py
+++ test_pylot.py
@@ -2,9 +2,15 @@ from sqlalchemy import Table, MetaData, Column, String, Integer, ForeignKey
 from sqlalchemy import create_engine
 from sqlalchemy.orm import mapper, sessionmaker
 
-
+# test_pylot.py is a bridge between Mozilla Test Pilot, a broader effort
+# to collect anonymous usage data, and eventually a dedicated data
+# pipeline for Mozilla's metrics team.
+#
+# Author: Matt DeBoard (http://mattdeboard.net)
 
 class Event(object):
+    '''Event is a prototype class, in case we wind up with some MVC/MVT
+    web framework apart from Test Pilot.'''
     
     def __init__(self, event_code, data1, data2, data3, timestamp):
         self.event_code = event_code
@@ -20,6 +26,9 @@ class Event(object):
                                                                 self.data3, 
                                                                 self.timestamp)
 
+class Report():
+
+    
 
 
 engine = create_engine("sqlite:///browser_usage_example_results.sqlite")
@@ -37,8 +46,14 @@ q = session.query(usage_patterns).order_by("timestamp")
 keys = ('event_code', 'data1', 'data2', 'data3', 'timestamp')
 events = [zip(keys,i) for i in q]
 dlist = []
+
+
+# Returns a list of dictionaries -- this belongs inside a class.
 for event in events:
     dlist.append(dict(event))
-
-
+event_stream = {}
+event_stream['events'] = dlist
+event_stream['iter'] += 1
+# Prep serial data for POST
+serial_data = json.dumps(event_stream, sort_keys=True, indent=4)
 
