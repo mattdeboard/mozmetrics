b4a468b95332a49604df2bf5d31f47b44824bab4
Matt DeBoard <matt.deboard@gmail.com>
2011-03-22 15:24:39 -0400

    Refactored, rounded out Store prototype. Still work to do here with required method overrides.

--- metrics.js
+++ metrics.js
@@ -2,6 +2,8 @@
  * Matt DeBoard (matt.deboard@gmail.com)
  */
 
+const EXPORTED_SYMBOLS = ["Metrics", "MetricsRecord"];
+
 const Cc = Components.classes;
 const Ci = Components.interfaces;
 const Cu = Components.utils;
@@ -11,136 +13,107 @@ Cu.import("resource://gre/modules/JSON.jsm");
 Cu.import("resource://services-sync/engines.js");
 Cu.import("resource://services-sync/record.js");
 Cu.import("resource://services-sync/util.js");
+Cu.import("resource://services-sync/engines/clients.js");
+// How do I do this properly?
+Cu.import("/home/matt/mozmetrics/daily_metrics.js");
 
 const METRICS_TTL = 604800; // 7 days
 
 function MetricsRecord(collection, id) {
-    CryptoWrapper.call(this, collection, id);
+  CryptoWrapper.call(this, collection, id);
 }
 
 MetricsRecord.prototype = {
-    __proto__: CryptoWrapper.prototype,
-    _logName: "Record.Metrics",
-    ttl: METRICS_TTL
+  __proto__: CryptoWrapper.prototype,
+  _logName: "Record.Metrics",
+  ttl: METRICS_TTL,
+  appName: Svc.AppInfo.name,
+  appVer: Svc.AppInfo.version,
+  appPlatVer: Svc.AppInfo.platformVersion
 };
 
 Utils.deferGetSet(MetricsRecord, "cleartext", ["event_code", "data1", "data2", 
 					       "data3", "timestamp"]);
 
+Utils.lazy(this, "Metrics", MetricsEngine);
+
 function MetricsEngine() {
-    SyncEngine.call(this, "Metrics");
+  SyncEngine.call(this, "Metrics");
 }
 
 MetricsEngine.prototype = {
-    __proto__: SyncEngine.prototype,
-    _recordObj: MetricsRecord,
-    _storeObj: MetricsStore,
-    _trackerObj: MetricsTracker
+  __proto__: SyncEngine.prototype,
+  _recordObj: MetricsRecord,
+  _storeObj: MetricsStore,
+  _trackerObj: MetricsTracker
 };
-    
+
 function MetricsStore(name) {
-    Store.call(this, name);
+  Store.call(this, name);
 }
 
 MetricsStore.prototype = {
-    __proto__: Store.prototype,
+  __proto__: Store.prototype,
+  
+  _initDB: function (fileName, tableName, columns) {
+    // These properties reflect the properties of the dataStoreInfo
+    // used by TestPilot originally, upon which this addon is heavily
+    // based.
+    this._fileName = fileName;
+    this._tableName = tableName;
+    this._columns = columns;
+    // Returns an object representing a connection to the metrics db
+    // client-side. This is step 1 in preparing a new record.
+    let storageSvc = Cc["@mozilla.org/storage/service;1"]
+      .getService(Ci.mozIStorageService);
+    let file = Cc["@mozilla.org/file/directory_service;1"]
+      .getService(Ci.nsIProperties)
+      .get("ProfD", Ci.nsIFile);
+    file.append(this._fileName);
+    return storageSvc.openDatabase(file);
+  },
+  
+  _stmt: null,
+  get stmt () {
+    // Creates an mozIStorageStatement object we can iterate through
+    // to create serialized data from the metrics db. A GUID will be added
+    // by _setGUID. This is step 2 in preparing a new record.
+    let openConn = this._initDB(dataStoreInfo.fileName, 
+				dataStoreInfo.tableName, 
+				dataStoreInfo.columns);
+    let now = Date.now();
+    let sqlQuery = "SELECT * FROM " + this._tableName + 
+      " WHERE timestamp > :last";
+    sqlQuery.params.last = this.lastSyncLocal;
+    return openConn.createStatement(sqlQuery);
+  },
+  
+  _metricsData: [],
+  get metricsData () {
+    /* Utils.queryAsync returns a {column_name: value} array from our metrics 
+     * db, so there is no need to write a custom mozIStorageService algorithm 
+     * here. This is step 3 in preparing a new record. */
+    let statement = this._stmt;
+    return Utils.queryAsync(statement, this._columns);
+  },
 
-    _initDB: function (fileName, tableName, columns) {
-	// These properties reflect the properties of the dataStoreInfo
-	// used by TestPilot originally, upon which this addon is heavily
-	// based.
-	this._fileName = fileName;
-	this._tableName = tableName;
-	this._columns = columns;
-	// Returns an object representing a connection to the metrics db
-	// client-side. 
-	let storageSvc = Cc["@mozilla.org/storage/service;1"]
-	    .getService(Ci.mozIStorageService);
-	let file = Cc["@mozilla.org/file/directory_service;1"]
-	    .getService(Ci.nsIProperties)
-	    .get("ProfD", Ci.nsIFile);
-	file.append(this._fileName);
-	let dbConn = storageSvc.openDatabase(file);
-	return dbConn;
-    },
-
-    _serialize: function (callback) {
-	/* _serialize creates a JSON data object, a "snapshot" of the database 
-	 * where the earliest entry is the first entry after the previous 
-	 * snapshot. */
-
-	// This is ugly. Definitely need to factor out the SQL query to select
-	// only rows that weren't included in the last update.
-	// lastSyncLocal is a built-in method of the SyncEngine superclass 
-	// defined in engines.js.
-	let now = Date.now();
-	let last = this.lastSyncLocal;
-	let sqlQuery = "SELECT * FROM " + this._tableName + 
-	    " WHERE timestamp > last";
-	let stmt = this._createStatement(sqlQuery);
-	let latestStats = [];
-	let numCols = stmt.columnCount;
-	let self = this;
-
-	stmt.executeAsync({
-	    handleResult: function(aResultSet) {
-		/* Python equiv (if numCols and stats were dicts) of:
-		 * 
-		 * for row in results:
-		 *   newStat = {}
-		 *   for i in numCols:
-		 *     column = self._columns[i]
-		 *     value = 0
-		 *     if column.type == "TYPE_INT_32":
-		 *       value = row.getInt32(i)
-		 *     elif column.type == "TYPE_DOUBLE":
-		 *       value = row.getDouble(i)
-		 *     else:
-		 *       value = sanitizeString(row.getUTF8String(i))
-		 *     newStat[column.name] = value
-		 *   latestStats[row.timestamp] = newStat
-		 * 
-		 * Pythonic pseudocode there obviously.
-		 */
-		for (let row = aResultSet.getNextRow(); row; 
-		     row = aResultSet.getNextRow()) {
-		    let newStat = [];
-		    for (let i = 0; i < numCols; i++) {
-			let column = self._columns[i];
-			let value = 0;
-			let colName = self._columns[i].property;
-			switch (column.type) {
-			    case TYPE_INT_32:
-			      value = row.getInt32(i);
-			    break;
-			    case TYPE_DOUBLE:
-			      value = row.getDouble(i);
-			    break;
-			    case TYPE_STRING:
-			      value = sanitizeString(row.getUTF8String(i));
-			    break;
-			}
-			/* Doing it thusly gives us a fairly easy-to-read data
-			 * set, ex.:
-			 * 
-			 * {"event_code": 26, "data1": "1", "data2": "3", 
-			 * "data3": "", "timestamp": 130020323}
-			 * 
-			 * This is untested as of 21Mar2011
-			 */
-			newStat[colName] = row.getResultByName(colName);
-		    }
-		    latestStats.push(newStat);
-		}
-	    },
-	    handleError: function(aError) {
-		callback(latestStats);
-	    },
-	    handleCompletion: function(aReason) {
-		callback(latestStats);
-	    }
-	});
-	stmt.finalize();
-    }
-};
+  _setGUID: [],
+  get setGUID () {
+    let stats = this._metricsData;
+    let guid = Utils.makeGUID();
+    return this._setGUID[guid] = stats;
+  },
+  
+  createRecord: function createRecord(id, collection) {
+    let record = new MetricsRecord(collection, id);
+    return record;
+  },
+  
+  itemExists: function itemExists(id) {
     
+
+    
+  }
+  
+};
+
